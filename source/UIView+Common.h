e1f27760b13b91a6e7af0659fe4ba050d505a213
gitwj <1057234592@qq.com>
2016-08-21 12:45:04 +0800

    add tog

--- source/UIView+Common.h
+++ source/UIView+Common.h
@@ -15,7 +15,7 @@
 @property (assign, nonatomic) CGFloat y;
 @property (assign, nonatomic) CGFloat width;
 @property (assign, nonatomic) CGFloat height;
-@property (assign, nonatomic) CGSize size;
+@property (assign, nonatomic) CGSize  size;
 @property (assign, nonatomic) CGPoint origin;
 @end
 
